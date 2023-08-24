cf4d168996c54f5db8e98548add57e0f7453a17c
Therese Otterhäll <tessan.otterhall@gmail.com>
2023-08-24 12:36:06 +0200

    woop roomlist funk

--- client/src/Components/Chat/Chat.tsx
+++ client/src/Components/Chat/Chat.tsx
@@ -1,21 +1,22 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useSocket } from "../../Context/SocketContext/socketContext";
 import RoomTemplate from "../Room/RoomTemplate";
+import RoomList from "../RoomList/RoomList";
 
 function Chat() {
-  const { setRoom, username, changeRoom } = useSocket();
+  const { setRoom, username, changeRoom, setCreatedRooms, createdRooms } =
+    useSocket();
   const [newRoomName, setNewRoomName] = useState("");
-  const [createdRooms, setCreatedRooms] = useState<string[]>([]);
 
-  const handleJoinRoom = (roomName: React.SetStateAction<string>) => {
+  const handleJoinRoom = (roomName: string) => {
     setRoom(roomName);
-    changeRoom("");
+    changeRoom(roomName);
   };
-
   const handleCreateRoom = () => {
     if (newRoomName.trim() !== "") {
       setCreatedRooms((prevRooms) => [...prevRooms, newRoomName]);
       handleJoinRoom(newRoomName);
+      setNewRoomName("");
     }
   };
 
@@ -34,18 +35,9 @@ function Chat() {
 
       <hr />
       <br />
-      <div>
-        <h2>Skapade rum:</h2>
-        <ul>
-          {createdRooms.map((roomName) => (
-            <li key={roomName}>
-              <button onClick={() => handleJoinRoom(roomName)}>
-                Join {roomName}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
+
+      <RoomList createdRooms={createdRooms} handleJoinRoom={handleJoinRoom} />
+
       <hr />
       <br />
 
@@ -55,3 +47,65 @@ function Chat() {
 }
 
 export default Chat;
+
+// import React, { useState } from "react";
+// import { useSocket } from "../../Context/SocketContext/socketContext";
+// import RoomTemplate from "../Room/RoomTemplate";
+// import RoomList from "../RoomList/RoomList";
+
+// function Chat() {
+//   const { setRoom, username, changeRoom } = useSocket();
+//   const [newRoomName, setNewRoomName] = useState("");
+//   const [createdRooms, setCreatedRooms] = useState<string[]>([]);
+
+//   const handleJoinRoom = (roomName: React.SetStateAction<string>) => {
+//     setRoom(roomName);
+
+//     changeRoom(newRoomName);
+//   };
+
+//   const handleCreateRoom = () => {
+//     if (newRoomName.trim() !== "") {
+//       setCreatedRooms((prevRooms) => [...prevRooms, newRoomName]);
+//       handleJoinRoom(newRoomName);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <p>Welcome to {username}! Please choose a chat room below:</p>
+
+//       <input
+//         type="text"
+//         value={newRoomName}
+//         onChange={(e) => setNewRoomName(e.target.value)}
+//         placeholder="Enter room name"
+//       />
+//       <button onClick={() => handleCreateRoom()}>Create and join room</button>
+//       <button onClick={() => handleJoinRoom("lobby")}>Join Lobby</button>
+
+//       <hr />
+//       <br />
+//       <div>
+//         <h2>Skapade rum:</h2>
+
+//         <ul>
+//           {createdRooms.map((roomName) => (
+//             <li key={roomName}>
+//               <button onClick={() => handleJoinRoom(roomName)}>
+//                 Join {roomName}
+//               </button>
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//       <hr />
+//       <br />
+
+//       <RoomTemplate />
+//       <RoomList />
+//     </div>
+//   );
+// }
+
+// export default Chat;
